15bc252283cbd8de77bf29685519fa2c6875ea03
JD <zelenuj@gmail.com>
2021-04-29 09:39:58 +0300

    CU-gf5th6 getNewRelationKey у фронт-моделі

--- upload/catalog/model/tool/jd_tools/relations.php
+++ upload/catalog/model/tool/jd_tools/relations.php
@@ -7,6 +7,13 @@ class ModelToolJDToolsRelations extends Model {
 		if(!empty($result->row['old_value'])) return $result->row['old_value'];
 		else return false;
 	}
+	public function getNewRelationKey($key, $value, $source) {
+		$sql = "SELECT `new_value` FROM `" . DB_PREFIX . "relations` WHERE `key` = '" . $key . "' AND `old_value` = '" . $value . "' AND `data_source` = '" . $source . "'";
+		$result = $this->db->query($sql);
+
+		if(!empty($result->row['new_value'])) return $result->row['new_value'];
+		else return false;
+	}
 	public function setRelationKey( $key, $old_value, $new_value, $source, $lastmode = '') {
 		$sql = "INSERT `" . DB_PREFIX. "relations` (`key`, `old_value`, `new_value`, `data_source`, `lastmode`) VALUE ('" . $key ."', '". $old_value ."', '". $new_value ."', '" . $source . "', " . (($lastmode == '')? 'NOW()' : "'" . $lastmode . "'" ) . ");";
 		$result = $this->db->query($sql);
