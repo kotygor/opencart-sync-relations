5e7c091e91b9efa657ba295a15a5c6c0db22548e
JD <zelenuj@gmail.com>
2021-09-01 08:57:48 +0300

    CU-kxd91e Додано метод getExistingRelations для отримання всіх збережених відношень

--- upload/catalog/model/tool/jd_tools/relations.php
+++ upload/catalog/model/tool/jd_tools/relations.php
@@ -15,6 +15,26 @@ class ModelToolJDToolsRelations extends Model {
 		if(!empty($result->row['new_value'])) return $result->row['new_value'];
 		else return false;
 	}
+
+	/**
+	 * @param $key, ключ-ід
+	 * @param $source, джерело
+	 * @param string[] $fields, пара ключ-значення
+	 * @return array
+	 */
+	public function getExistingRelations($key, $source, $fields = ['key' => 'old_value', 'value' => 'new_value']) {
+		$sql = "SELECT `{$fields['key']}`, `{$fields['value']}` FROM `" . DB_PREFIX . "relations`"
+			. " WHERE `key` = '" . $key . "' AND `data_source` = '" . $source . "'";
+		$rows = $this->db->query($sql)->rows;
+
+		$result = [];
+		foreach ($rows as $row) {
+			$result[$row[$fields['key']]] = $row[$fields['value']];
+		}
+
+		return $result;
+	}
+
 	public function setRelationKey( $key, $old_value, $new_value, $source, $lastmode = '') {
 		$sql = "INSERT `" . DB_PREFIX. "relations` (`key`, `old_value`, `new_value`, `data_source`, `lastmode`) VALUE ('" . $key ."', '". $old_value ."', '". $new_value ."', '" . $source . "', " . (($lastmode == '')? 'NOW()' : "'" . $lastmode . "'" ) . ");";
 		$result = $this->db->query($sql);
